e2be9986a4b26bce114ec5e2e3d77d4102097da5
zldtmskdlxm <zldtmskdlxm@gmail.com>
2020-11-06 17:36:29 +0900

    CSS 강의

---
+++ HTML/ch01/js/js_01.java
@@ -0,0 +1,16 @@
+//1. 자바 스크립트는 자료형이 동적으로 정해진다
+
+var num = 10;           //정수      //1Byte
+
+var str = "문자열";     //문자열    //9Byte
+
+var floatNum = 10.5;    //실수      //8Byte
+
+var charStr = "가";     //문자      //3Byte
+
+var boolDate = true;    //bool(참거짓) true, false  //1Byte
+
+var user = {
+    name : "홍길동";
+    phone : "01022225555"
+}
